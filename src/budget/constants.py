37f17a03814c2cf038339f25827e00053ebb2746
deterministic-nonperiodic <morfayanmichel@gmail.com>
2025-10-20 22:58:36 +0200

    added constants

---
+++ src/budget/constants.py
@@ -0,0 +1,7 @@
+# Constants
+g = 9.80665  # gravitational acceleration (m / s**2)
+cp = 1004.0  # specific heat at constant pressure for dry air (J / kg / K)
+ps = 1000e2  # reference surface pressure (Pa)
+Rd = 287.058  # gas constant for dry air (J / kg / K)
+chi = Rd / cp  # ~2/7 (dimensionless)
+earth_radius = 6.3712e6  # mean Earth radius (m)
